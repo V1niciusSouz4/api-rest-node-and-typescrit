9973c2058aeedfafb9679f134e4f6383c428f0ee
Vinícius Souza <viniciussouzasantos2004@gmail.com>
2023-06-01 10:56:08 -0300

    feat: added folders structure

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -5,6 +5,7 @@ const router = Router();
 router.get('/', (req, res) => {
     return res.json('Olá, DEV!');
 });
+
 router.post('/teste', (req, res) => {
     return res.status(401).json(req.body);
 });
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,5 @@
 import express, { json } from 'express';
-import { router } from '../routes/index';
+import { router } from './routes/index';
 import 'dotenv/config';
 
 const server = express();
